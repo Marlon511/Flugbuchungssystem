53269938ad374d5eb3f7760e6f77e0fc498059da
83470 <83470@DE-W11-SN-40919>
2026-05-12 14:41:25 +0200

    Implementing Class "Passagier"

--- Projektarbeit-Flugbuchungssystem/src/de/Flugbuchungssystem/stammdaten/Passagier.java
+++ Projektarbeit-Flugbuchungssystem/src/de/Flugbuchungssystem/stammdaten/Passagier.java
@@ -1,6 +1,45 @@
 package de.Flugbuchungssystem.stammdaten;
 
 public class Passagier {
-//test
-	//test2
+
+	private String vorname;
+	private String nachname;
+    private String email;
+    private String telefonnummer;
+
+    public Passagier(String vorname, String nachname, String email, String telefonnummer) {
+
+    		this.vorname = vorname;
+    		this.nachname = nachname;
+    		this.telefonnummer = telefonnummer;
+    		this.email = email;
+
+
+    		}
+
+    public String getVorname() {
+    		return this.vorname;
+    		}
+
+    public String getNachname() {
+    		return this.nachname;
+    		}
+
+    public String getEmail() {
+    		return this.email;
+    		}
+
+    public String getTelefonnummer() {
+	        return this.telefonnummer;
+    		}
+
+    public String getVollerName() {
+	        return this.vorname + " " + this.nachname;
+	    }
+
+    public String toString() {
+    		return getVollerName() + 
+    				" | " + this.email + 
+    				" | " + this.telefonnummer;
+    		}
 }
