310842af7fa67077d705eef11827294870c21c29
83470 <83470@DE-W11-SN-40919>
2026-05-13 18:37:09 +0200

    Hinzufügen von JavaDoc zum Enum "Buchungsstatus"

--- Projektarbeit-Flugbuchungssystem/src/de/Flugbuchungssystem/model/Buchungsstatus.java
+++ Projektarbeit-Flugbuchungssystem/src/de/Flugbuchungssystem/model/Buchungsstatus.java
@@ -1,8 +1,11 @@
 package de.Flugbuchungssystem.model;
-
+/**
+ * Beschreibt den Lebenszyklus einer Buchung.
+ * Eine neue Buchung beginnt immer mit dem Status {@link #GEBUCHT}.
+ */
 public enum Buchungsstatus {
-	GEBUCHT,
-	UMGEBUCHT,
-	STORNIERT;
+	GEBUCHT, 	/** Die Buchung ist aktiv und gültig. */
+	UMGEBUCHT, 	/** Die Buchung wurde auf einen anderen Flug umgebucht. */
+	STORNIERT; 	/** Die Buchung wurde storniert; der Sitz ist wieder verfügbar. */
 
 }
